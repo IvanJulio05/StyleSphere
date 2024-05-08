10d21d1c9788907c25de1560aa7645662beaea80
kourugane <rodriguezbarrerafabiandavid@gmail.com>
2024-05-08 12:23:51 -0500

    Agrege la fuente LA pantalla con el formilario de vender Y un estilo para los edidtext

--- app/src/main/java/com/example/tiendaropa/Vender.java
+++ app/src/main/java/com/example/tiendaropa/Vender.java
@@ -1,8 +1,12 @@
 package com.example.tiendaropa;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Vender extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class Vender extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vender);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.opciones, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedOption = parent.getItemAtPosition(position).toString();
+
+                Toast.makeText(Vender.this, "Opción seleccionada: " + selectedOption, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
